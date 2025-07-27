ee10cbcde0b5ab0a6c16278fa19fd022e35b50dd
Thomas M <thomasm6m6@gmail.com>
2025-07-27 16:23:43 -0700

    Revert accidental change

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,8 @@ export default function App() {
       <MapView
         ref={mapRef}
         initialCenter={initialCenter}
+        realtimeHighlighting={realtimeHighlighting}
+        showCachedPolygons={showCachedPolygons}
       >
         <PathPlan xml={taskXml}/>
       </MapView>
